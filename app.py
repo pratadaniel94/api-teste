4b1eb75078bb486b40d3c788d7a163508f8a2574
Daniel Prata da Silva <danielprata@outlook.com>
2018-12-11 18:52:17 -0200

    api-teste

--- app.py
+++ app.py
@@ -1,1 +1,11 @@
-from flask import Flask
+from flask import Flask, jsonify
+
+
+app = Flask(__name__)
+
+@app.route('/<string:nome>', methods=["GET"])
+def index(nome):
+    return jsonify({'nome':nome})
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', debug=True, port=5000)
