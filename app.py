beb1e47d482482470aaed5d2c5873e2a07daae4a
Daniel Prata da Silva <danielprata@outlook.com>
2018-12-12 20:03:21 -0200

    finish api

--- app.py
+++ app.py
@@ -8,15 +8,19 @@ except Exception as e:
     print(e)
 
 app = Flask(__name__)
-def validar_json(json):
+
+
+def validar_json(json, method="GET"):
     if json:
         try:
             if json['nome'] and json['_id']:
-                db.usuarios.insert(json)
                 return True
             else:
                 return False
         except KeyError:
+            if method == "PUT":
+                if json['nome']:
+                    return True
             return False
     else:
         return False
@@ -24,27 +28,26 @@ def validar_json(json):
 
 @app.route('/usuarios', methods=["GET", "POST"])
 def get_or_post_users():
-    if request.method ==  "GET":
+    if request.method == "GET":
         return jsonify(list(db.usuarios.find()))
     else:
         data = request.get_json()
         if isinstance(data, dict):
-            status = validar_json(data)
-            return jsonify({"status": status})
-        elif isinstance(data,list):
-            for registro in data:
-                if not validar_json(registro):
-                    return jsonify({"status": False})
 
+            if validar_json(data):
+                db.usuarios.insert(data)
+                return jsonify({"status": True})
+            else:
+                return jsonify({"status": False})
+        elif isinstance(data, list):
+            for registro in data:
+                if validar_json(registro):
+                    db.usuarios.insert(registro)
+                else:
+                    return jsonify({"status": False})
             else:
                 return jsonify({"status": True})
 
-
-# @app.route('/usuarios', methods=['POST' ])
-# def register_users():
-#     print(request.get_json())
-#     print(dir(request))
-#     return 'sucess'
 
 @app.route('/usuarios/<string:busca>', methods=["GET"])
 def get_user(busca):
@@ -54,10 +57,22 @@ def get_user(busca):
         return jsonify(list(db.usuarios.find({"nome": busca.lower().strip()})))
 
 
-# @app.route('/<string:nome>', methods=["GET"])
-# def index(nome):
-    # return jsonify({'nome':nome})
+@app.route('/usuario/<int:id>', methods=['PUT', 'DELETE'])
+def update_user(id):
+    if request.method == "PUT":
+        data = request.get_json()
+        if validar_json(data, method="PUT"):
+            db.usuarios.update({"_id":id},{"$set":data})
+            return jsonify({"status":True})
+        else:
+            return jsonify({"status": False})
+    elif request.method == "DELETE":
+        try:
+            db.usuarios.remove({"_id":id})
+            return jsonify({"status": True})
+        except Exception:
+            return jsonify({"status": False})
 
-
+    
 if __name__ == "__main__":
     app.run(host='0.0.0.0', debug=True, port=5000)
